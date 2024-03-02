d2f0b0621da6b97e8bd11222f0a8321b3334e872
Pooya <83201335+iamcraz@users.noreply.github.com>
2024-03-02 10:57:31 +0330

    Areas V.01

---
+++ Areas/Admin/Controllers/PageGroupsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Datalayer;
+
+namespace MyFirsyProjectWebApp.Areas.Admin.Controllers
+{
+    public class PageGroupsController : Controller
+    {
+        private IPageGroupRepository pageGroupRepository;
+
+        // GET: Admin/PageGroups
+        MyFirstWebAppContext db = new MyFirstWebAppContext();
+        public PageGroupsController()
+        {
+            pageGroupRepository = new PageGroupRepository(db);
+            
+        }
+        public ActionResult Index()
+        {
+            return View(pageGroupRepository.GetAllGroups());
+        }
+
+        // GET: Admin/PageGroups/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PageGroup pageGroup = pageGroupRepository.GetGroupByID(id.Value);
+            if (pageGroup == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView(pageGroup);
+        }
+
+        // GET: Admin/PageGroups/Create
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+
+        // POST: Admin/PageGroups/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "GroupID,GroupTitle")] PageGroup pageGroup)
+        {
+            if (ModelState.IsValid)
+            {
+                pageGroupRepository.Insert(pageGroup);
+                pageGroupRepository.save();
+                return RedirectToAction("Index");
+            }
+
+            return View(pageGroup);
+        }
+
+        // GET: Admin/PageGroups/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PageGroup pageGroup = pageGroupRepository.GetGroupByID(id.Value);
+            if (pageGroup == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView(pageGroup);
+        }
+
+        // POST: Admin/PageGroups/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "GroupID,GroupTitle")] PageGroup pageGroup)
+        {
+            if (ModelState.IsValid)
+            {
+                pageGroupRepository.Uptade_group(pageGroup);
+                pageGroupRepository.save();
+                return RedirectToAction("Index");
+            }
+            return View(pageGroup);
+        }
+
+        // GET: Admin/PageGroups/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PageGroup pageGroup = pageGroupRepository.GetGroupByID(id.Value);
+            if (pageGroup == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView(pageGroup);
+        }
+
+        // POST: Admin/PageGroups/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            pageGroupRepository.Delete(id);
+            pageGroupRepository.save();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                pageGroupRepository.Dispose();
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;   
+using System.Web;
+using System.Web.Mvc;
+using Datalayer;
+
+namespace MyFirsyProjectWebApp.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        private IPageRepositroy pageRepositroy;
+        private IPageGroupRepository pageGroupRepository;
+        private MyFirstWebAppContext db = new MyFirstWebAppContext();
+
+        public PagesController()
+        {
+            pageRepositroy = new PageRepository(db);
+            pageGroupRepository = new PageGroupRepository(db);
+        }
+
+        // GET: Admin/Pages
+        public ActionResult Index()
+        {
+
+            return View(pageRepositroy.GetAllPages());
+        }
+
+        // GET: Admin/Pages/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Page page = pageRepositroy.GetPageByID(id.Value);
+            if (page == null)
+            {
+                return HttpNotFound();
+            }
+            return View(page);
+        }
+
+        // GET: Admin/Pages/Create
+        public ActionResult Create()
+        {
+            ViewBag.GroupID = new SelectList(pageGroupRepository.GetAllGroups(), "GroupID", "GroupTitle");
+            return View();
+        }
+
+        // POST: Admin/Pages/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PageID,GroupID,Title,ShortDiscription,Text,Visit,ImageName,ShowInSlider,CreateDate")] Page page,HttpPostedFileBase imgUp)
+        {
+            if (ModelState.IsValid)
+            {
+                page.Visit = 0;
+                page.CreateDate = DateTime.Now;
+                
+                if (imgUp != null)
+                {
+                    page.ImageName = Guid.NewGuid() + Path.GetExtension(imgUp.FileName);
+                    imgUp.SaveAs(Server.MapPath("/PageImages/" + page.ImageName));
+                }
+
+
+                pageRepositroy.Insert(page);
+                pageRepositroy.save();//////// دقیقا تا همینجا جلو رفتیم دقیقه 44 ویدیو و قراره ک عکس رو ذخیره کنیم//////
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.GroupID = new SelectList(pageGroupRepository.GetAllGroups(), "GroupID", "GroupTitle", page.GroupID);
+            return View(page);
+        }
+
+        // GET: Admin/Pages/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Page page = pageRepositroy.GetPageByID(id.Value);
+            if (page == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.GroupID = new SelectList(pageGroupRepository.GetAllGroups(), "GroupID", "GroupTitle", page.GroupID);
+            return View(page);
+        }
+
+        // POST: Admin/Pages/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PageID,GroupID,Title,ShortDiscription,Text,Visit,ImageName,ShowInSlider,CreateDate")] Page page,HttpPostedFileBase imgUp)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                if (imgUp != null)
+                {
+                    if (page.ImageName != null)
+                    {
+                        System.IO.File.Delete(Server.MapPath("/PageImages/" + page.ImageName));
+                    }
+                    page.ImageName = Guid.NewGuid() + Path.GetExtension(imgUp.FileName);
+                    imgUp.SaveAs(Server.MapPath("/PageImages/" + page.ImageName));
+                }
+
+                pageRepositroy.Uptade(page);
+                pageRepositroy.save();
+                return RedirectToAction("Index");
+            }
+            ViewBag.GroupID = new SelectList(db.PageGroups, "GroupID", "GroupTitle", page.GroupID);
+            return View(page);
+        }
+
+        // GET: Admin/Pages/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Page page = pageRepositroy.GetPageByID(id.Value);
+            if (page == null)
+            {
+                return HttpNotFound();
+            }
+            return View(page);
+        }
+
+        // POST: Admin/Pages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Page page = pageRepositroy.GetPageByID(id);
+            if (page.ImageName != null)
+            {
+                System.IO.File.Delete(Server.MapPath("/PageImages/" + page.ImageName));
+            }
+            pageRepositroy.Delete(page);
+            pageRepositroy.save();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                pageGroupRepository.Dispose();
+                pageRepositroy.Dispose();
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
